60ca05dbd8e50c9e6ee32de4264b002e822debe3
Illya9999 <65667527+Illya9999@users.noreply.github.com>
2020-07-22 12:02:39 -0400

    Add sandbox hats

--- src/managers/Store.js
+++ src/managers/Store.js
@@ -2,7 +2,7 @@ var shop = require('../utils/shopItems');
 
 class Store {
 	canBuyItem(socket, item) {
-		return (socket.player.points >= item.price);
+		return (socket.player.points >= item.price || global.sandbox);
 	}
 	getItemById(id, isTail = !1) {
 		var me = this;
