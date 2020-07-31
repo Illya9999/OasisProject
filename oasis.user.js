c65eedcec7d2935d3eead9fb5ccfef4a1621f4d2
Illya9999 <65667527+Illya9999@users.noreply.github.com>
2020-07-31 14:57:07 -0400

    Update oasis.user.js

--- oasis.user.js
+++ oasis.user.js
@@ -8,7 +8,7 @@
     let ws = window.WebSocket;
     class OasisSocket extends ws {
         constructor(){
-            super('ws://127.0.0.1:5000');
+            super('ws://127.0.0.1:3000');
         }
     }
     window.WebSocket = OasisSocket;
